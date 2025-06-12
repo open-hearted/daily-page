346e9128951f8701032af38faa492053e28b24d6
open-hearted <49687225+open-hearted@users.noreply.github.com>
2025-06-13 02:58:20 +0900

    Update generate_page.py

--- generate_page.py
+++ generate_page.py
@@ -4,14 +4,21 @@ import os
 JST = timezone(timedelta(hours=9))
 now = datetime.now(JST)
 date_str = now.strftime("%Y-%m-%d")
-# time_str = now.strftime("%H:%M:%S")
+# 0=月曜, …, 6=日曜
+weekday_index = now.weekday()
+
+# 日本語の曜日リストを用意
+weekday_list = ["月", "火", "水", "木", "金", "土", "日"]
+
+# 最終的に「金曜日」などの文字列に
+weekday_str = weekday_list[weekday_index]
 affirmation = "今日も一歩前へ進もう"
 
 os.makedirs("logs", exist_ok=True)
 with open(f"logs/{date_str}.html", "w", encoding="utf-8") as f:
     f.write(f"""
     <html><body>
-    <h1>{date_str}</h1>
+    <h1>{date_str} {weekday_str}</h1>
     <p>{affirmation}</p>
     </body></html>
     """)
@@ -19,7 +26,7 @@ with open(f"logs/{date_str}.html", "w", encoding="utf-8") as f:
 with open("index.html", "w", encoding="utf-8") as f:
     f.write(f"""
     <html><body>
-    <h1>{date_str}</h1>
+    <h1>{date_str}  {weekday_str}</h1>
     <p>{affirmation}</p>
     <p><a href="logs/{date_str}.html">この日の記録を見る</a></p>
     </body></html>
